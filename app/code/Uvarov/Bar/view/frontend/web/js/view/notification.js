326b5c06da7603c39be24ee1129638993d83960f
vova <uvarov.vova@gmail.com>
2019-01-17 13:52:11 +0200

    knockout js without block

---
+++ app/code/Uvarov/Bar/view/frontend/web/js/view/notification.js
@@ -0,0 +1,39 @@
+define([
+	'uiComponent',
+	'ko',
+	'mage/url',
+	'mage/storage'
+], function (Component, ko, urlBuilder, storage) {
+	return Component.extend({
+		initialize: function () {
+			this._super();
+			this.populateUi();
+			this.gameplay();
+
+			this.getNotifications();
+			this.loadNotifocations();
+		},
+		getNotifications: function () {
+
+			this.notifications = ko.observable();
+
+		},
+		loadNotifocations: function () {
+
+			var self = this;
+			var url = urlBuilder.build('notifications/index/notifications');
+
+			return storage.post(url, '')
+				.done(
+					function (responce) {
+						self.notifications(responce.items);
+					}
+				).fail(
+					function (responce) {
+						console.log(responce);
+					}
+				)
+
+		}
+	});
+});
